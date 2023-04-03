732ab5f0c33d12b27f5328aebc2a30190758c346
Vasiliy Baranovsky <remindscope@gmail.com>
2023-04-03 14:09:01 +0300

    Homework 5 minor refactoring

--- src/main/java/io/lesson04/eventsourcing/api/ApiApp.java
+++ src/main/java/io/lesson04/eventsourcing/api/ApiApp.java
@@ -3,22 +3,24 @@ package io.lesson04.eventsourcing.api;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 
 public class ApiApp {
-  public static void main(String[] args) {
-    try {
-      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-      PersonApi personApi = context.getBean(PersonApi.class);
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        PersonApi personApi = context.getBean(PersonApi.class);
+        try {
+            personApi.savePerson(1L, "Ivan", "Baraban", "Lol");
+            personApi.savePerson(1L, "Ivan", "Baraban", "Edit");
+            personApi.savePerson(2L, "Sergey", "Sax", "Tenor");
+            personApi.savePerson(3L, "Tamara", "Tuba", "Big");
+            personApi.deletePerson(1L);
 
-      personApi.savePerson(1L, "Ivan", "Baraban", "Lol");
-      personApi.savePerson(1L, "Ivan", "Baraban", "Edit");
-      personApi.savePerson(2L, "Sergey", "Sax", "Tenor");
-      personApi.savePerson(3L, "Tamara", "Tuba", "Big");
-      personApi.deletePerson(1L);
-
-    } catch (Exception e) {
-      e.printStackTrace();
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        context.close();
     }
-  }
-
 }
